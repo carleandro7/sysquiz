46a1c6222690dd7bd1f0ad0ff308f3b46e2f2521
carleandro7 <carleandro7@gmail.com>
2025-09-17 08:40:59 -0300

    Refactor JavaScript imports; update CKEditor and Cocoon script inclusion in admin layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "cocoon-js"
-import $ from "jquery";
-window.$ = $;
-window.jQuery = $;
+import "cocoon"
+import $ from "jquery"
+window.jQuery = $
 import * as ActiveStorage from "@rails/activestorage"
 
 
